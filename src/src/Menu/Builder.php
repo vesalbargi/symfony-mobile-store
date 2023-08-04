d82060eb805c1516e9e6e28c091ab0611c521f15
Vesal <vesalbargi81@gmail.com>
2023-08-04 21:57:03 +0330

    Customize knp menu using twig navbar template and bootstrap

--- src/src/Menu/Builder.php
+++ src/src/Menu/Builder.php
@@ -3,14 +3,18 @@
 namespace App\Menu;
 
 use App\Entity\MobileCompany;
+use App\Entity\MobilePhone;
 use Doctrine\ORM\EntityManagerInterface;
 use Knp\Menu\FactoryInterface;
 use Knp\Menu\ItemInterface;
 use Symfony\Bundle\SecurityBundle\Security;
-use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerAwareTrait;
 
-class Builder
+class Builder implements ContainerAwareInterface
 {
+    use ContainerAwareTrait;
+
     private FactoryInterface $factory;
     private EntityManagerInterface $entityManager;
     private Security $security;
@@ -22,32 +26,75 @@ class Builder
         $this->security = $security;
     }
 
-    public function mainMenu(RequestStack $requestStack): ItemInterface
+    public function mainMenu(array $options): ItemInterface
+    {
+        $menu = $this->factory->createItem('root');
+
+        $menu->addChild('About Us', ['route' => 'app_home'])
+            ->setAttribute('class', 'nav-item')
+            ->setLinkAttribute('class', 'nav-link');
+
+        $menu->addChild('Mobile Companies', ['route' => 'app_mobile_company_index'])
+            ->setAttribute('class', 'nav-item dropdown')
+            ->setLinkAttribute('class', 'nav-link dropdown-toggle')
+            ->setLinkAttribute('data-bs-toggle', 'link');
+
+        $mobileCompanies = $this->entityManager->getRepository(MobileCompany::class)->findAll();
+        foreach ($mobileCompanies as $mobileCompany) {
+            $menu['Mobile Companies']->addChild($mobileCompany->getName(), [
+                'route' => 'app_mobile_company_show',
+                'routeParameters' => ['id' => $mobileCompany->getId()]
+            ])
+                ->setLinkAttribute('class', 'dropdown-item');
+        }
+
+        $menu['Mobile Companies']->setChildrenAttribute('class', 'dropdown-menu dropdown-menu-dark');
+
+        $menu->addChild('Mobile Phones', ['route' => 'app_mobile_phone_index'])
+            ->setAttribute('class', 'nav-item dropdown')
+            ->setLinkAttribute('class', 'nav-link dropdown-toggle')
+            ->setLinkAttribute('data-bs-toggle', 'link');
+
+        $mobilePhones = $this->entityManager->getRepository(MobilePhone::class)->findAll();
+        foreach ($mobilePhones as $mobilePhone) {
+            $menu['Mobile Phones']->addChild($mobilePhone->getModel(), [
+                'route' => 'app_mobile_phone_show',
+                'routeParameters' => ['id' => $mobilePhone->getId()]
+            ])
+                ->setLinkAttribute('class', 'dropdown-item');
+        }
+
+        $menu['Mobile Phones']->setChildrenAttribute('class', 'dropdown-menu dropdown-menu-dark');
+
+        $menu->setChildrenAttribute('class', 'navbar-nav');
+
+        return $menu;
+    }
+
+    public function navbarMenu(array $options): ItemInterface
     {
         $menu = $this->factory->createItem('root');
 
         if ($this->security->isGranted('ROLE_ADMIN')) {
-            $menu->addChild('Admin', ['route' => 'admin']);
+            $menu->addChild('Admin', ['route' => 'admin'])
+                ->setAttribute('class', 'nav-item')
+                ->setLinkAttribute('class', 'nav-link');
         }
 
         if ($this->security->isGranted('IS_AUTHENTICATED_REMEMBERED')) {
-            $menu->addChild('Log out', ['route' => 'app_logout']);
+            $menu->addChild('Log out', ['route' => 'app_logout'])
+                ->setAttribute('class', 'nav-item')
+                ->setLinkAttribute('class', 'nav-link');
         } else {
-            $menu->addChild('Login', ['route' => 'app_login']);
-            $menu->addChild('Register', ['route' => 'app_register']);
+            $menu->addChild('Login', ['route' => 'app_login'])
+                ->setAttribute('class', 'nav-item')
+                ->setLinkAttribute('class', 'nav-link');
+            $menu->addChild('Register', ['route' => 'app_register'])
+                ->setAttribute('class', 'nav-item')
+                ->setLinkAttribute('class', 'nav-link');
         }
 
-        $menu->addChild('About Us', ['route' => 'app_home']);
-        $menu->addChild('Mobile Companies', ['route' => 'app_mobile_company_index']);
-
-        $hotels = $this->entityManager->getRepository(MobileCompany::class)->findAll();
-
-        foreach ($hotels as $hotel) {
-            $menu['Mobile Companies']->addChild($hotel->getName(), [
-                'route' => 'app_mobile_company_show',
-                'routeParameters' => ['id' => $hotel->getId()]
-            ]);
-        }
+        $menu->setChildrenAttribute('class', 'navbar-nav ms-auto');
 
         return $menu;
     }
